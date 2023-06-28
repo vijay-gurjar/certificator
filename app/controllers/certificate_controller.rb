dc632b1aa0ef2192e5e7ec8752b04830630f9e01
Vinay Gurjar <vinaychoudhary3@ymail.com>
2023-06-29 02:13:59 +0530

    "add admin dash board"

--- app/controllers/certificate_controller.rb
+++ app/controllers/certificate_controller.rb
@@ -24,6 +24,7 @@ class CertificateController < ApplicationController
   end
   
   def new
+    # "google-site-verification=EQo2itG2fMKjCdPrAZjnaaNPrKTDTGM5kDUv00upxWc"
   end
 
   def create_certificate
