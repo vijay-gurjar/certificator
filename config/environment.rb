e7261a7825a1b9933bea354347f5c337e6966c38
Vinay-Gurjar <103022724+Vinay-Gurjar@users.noreply.github.com>
2023-09-21 02:25:31 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.configure do
+...
+# returns error saying to add "rails_example" to config.hosts
+config.hosts << "rails_example"
+...
+end
