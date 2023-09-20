df1c281d7622ae3ab9ba9d17cb27de187d5e1957
Vinay-Gurjar <103022724+Vinay-Gurjar@users.noreply.github.com>
2023-09-21 02:25:45 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,6 @@ Rails.application.initialize!
 Rails.application.configure do
 ...
 # returns error saying to add "rails_example" to config.hosts
-config.hosts << "rails_example"
+config.hosts << "ec2-54-80-44-224.compute-1.amazonaws.com"
 ...
 end
