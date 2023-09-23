235ccbd1136616bbfef4f5a019ab7888c95b777a
Vinay-Gurjar <103022724+Vinay-Gurjar@users.noreply.github.com>
2023-09-23 15:24:13 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-    config.hosts << "ec2-54-80-44-224.compute-1.amazonaws.com"
+    config.hosts << "www.modimitra.in"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
